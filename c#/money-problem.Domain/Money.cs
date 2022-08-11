c4384a448c63439c83d35a9f185d4a01b53509f4
Cédric Hulin <hulin.cedric@gmail.com>
2022-08-11 13:56:43 +0200

    refactor(step3): Replace MoneyCalculator behavior by Money

---
+++ c#/money-problem.Domain/Money.cs
@@ -0,0 +1,10 @@
+﻿namespace money_problem.Domain;
+
+public readonly record struct Money(double Amount, Currency Currency)
+{
+    public Money Times(int times)
+        => this with { Amount = Amount * times };
+
+    public Money Divide(int divisor)
+        => this with { Amount = Amount / divisor };
+}
