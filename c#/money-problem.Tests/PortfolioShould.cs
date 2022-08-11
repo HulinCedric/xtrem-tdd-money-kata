f22ea1e90f72c15c05f1f1b8decdac9b02066299
Cédric Hulin <hulin.cedric@gmail.com>
2022-08-11 10:14:29 +0200

    feat(step2): 5 USD + 10 EUR = 17 USD

---
+++ c#/money-problem.Tests/PortfolioShould.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using money_problem.Domain;
+using Xunit;
+
+namespace money_problem.Tests;
+
+public class PortfolioShould
+{
+    private readonly Bank bank = Bank.WithExchangeRate(Currency.EUR, Currency.USD, 1.2);
+
+    [Fact(DisplayName = "5 USD + 10 EUR = 17 USD")]
+    public void AddMoneyInDollarAndEuro()
+    {
+        // Arrange
+        var portfolio = new Portfolio();
+        portfolio.Add(5, Currency.USD);
+        portfolio.Add(10, Currency.EUR);
+
+        // Act
+        var evaluation = portfolio.Evaluate(bank, Currency.USD);
+
+        // Assert
+        evaluation.Should().Be(17);
+    }
+}
+
+public class Portfolio
+{
+    public void Add(double amount, Currency currency)
+    {
+    }
+
+    public double Evaluate(Bank bank, Currency currency)
+        => 17;
+}
