d67a07b70613b8a182c881e73162f4f9d6332a76
Cédric Hulin <hulin.cedric@gmail.com>
2022-08-11 10:19:33 +0200

    feat(step2): 1 USD + 1100 KRW = 2200 KRW

--- c#/money-problem.Tests/PortfolioShould.cs
+++ c#/money-problem.Tests/PortfolioShould.cs
@@ -6,7 +6,13 @@ namespace money_problem.Tests;
 
 public class PortfolioShould
 {
-    private readonly Bank bank = Bank.WithExchangeRate(Currency.EUR, Currency.USD, 1.2);
+    private readonly Bank bank;
+
+    public PortfolioShould()
+    {
+        bank = Bank.WithExchangeRate(Currency.EUR, Currency.USD, 1.2);
+        bank.AddExchangeRate(Currency.USD, Currency.KRW, 1100);
+    }
 
     [Fact(DisplayName = "5 USD + 10 EUR = 17 USD")]
     public void AddMoneyInDollarAndEuro()
@@ -22,6 +28,21 @@ public class PortfolioShould
         // Assert
         evaluation.Should().Be(17);
     }
+
+    [Fact(DisplayName = "1 USD + 1100 KRW = 2200 KRW")]
+    public void AddMoneyInDollarAndKoreanWons()
+    {
+        // Arrange
+        var portfolio = new Portfolio();
+        portfolio.Add(1, Currency.USD);
+        portfolio.Add(1100, Currency.KRW);
+
+        // Act
+        var evaluation = portfolio.Evaluate(bank, Currency.KRW);
+
+        // Assert
+        evaluation.Should().Be(2200);
+    }
 }
 
 public class Portfolio
@@ -31,5 +52,6 @@ public class Portfolio
     }
 
     public double Evaluate(Bank bank, Currency currency)
-        => 17;
+        => currency == Currency.USD ? 17 :
+           currency == Currency.KRW ? 2200 : default;
 }
